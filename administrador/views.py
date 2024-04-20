2fa4e8585af20a1e31f4257b9139d3a201d95e1e
Carolina Moran <carolinacsm14@gmail.com>
2024-04-20 16:28:25 -0500

    casi

--- administrador/views.py
+++ administrador/views.py
@@ -66,7 +66,18 @@ def editar_bloque(request, bloque_id):
     if request.method == 'POST':
         form = BloqueForm(request.POST, request.FILES, instance=bloque)
         if form.is_valid():
-            # Save the form and handle the image
+            form.save()
+            return redirect('administrador')
+    else:
+        form = BloqueForm(instance=bloque)
+    return render(request, 'editar_bloque.html', {'form': form})
+
+@login_required
+def editar_bloque(request, bloque_id):
+    bloque = get_object_or_404(Bloque, id=bloque_id)
+    if request.method == 'POST':
+        form = BloqueForm(request.POST, request.FILES, instance=bloque)
+        if form.is_valid():
             form.save()
             return redirect('administrador')
     else:
